194637b28720dbc8e452778f5804dbd3499f5b94
Wegis Silveira <wegis.h.silveira@gmail.com>
2020-08-10 15:50:08 -0300

    Primeiro commit

---
+++ src/containers/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React, {Component, Fragment} from 'react'
+
+class Layout extends Component {
+
+
+    render () {
+        return (
+
+            <Fragment>
+                <nav className="navbar navbar-light bg-light">
+                    <a href="#" className="navbar-brand text-danger">myHome</a>
+                </nav>
+            </Fragment>
+        )
+    }
+}
+
+export default Layout
