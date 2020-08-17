3c235f3528f67782c1f39ba570cd2d4961b13b07
Wegis Silveira <wegis.h.silveira@gmail.com>
2020-08-17 11:58:45 -0300

    Commit meio do dia

--- src/components/Shop/ProductCard/ProductCard.js
+++ src/components/Shop/ProductCard/ProductCard.js
@@ -7,10 +7,10 @@ const productCard = props => {
 
     return (
 
-        <div className="session-container">
+        <div className="session-container mt-5">
             <div className="product-card-container container-fluid d-flex">
-                <div className="col-6">
-                    <div>
+                <div className="col-6 pl-0 pr-5">
+                    <div className="main-img-slider">
                         <img src={require('../../../assets/images/ProductCard/img-1.png')} alt="img-1" />
                         <div className="d-flex justify-content-between change-slide-setas-corrigir-nome">
                             <FontAwesomeIcon icon="arrow-left" />
@@ -23,25 +23,24 @@ const productCard = props => {
                         <img src={require('../../../assets/images/ProductCard/img-3.png')} alt="img-3" style={{maxWidth: '30%', maxHeight: '30%'}} />
                     </div>
                 </div>
-                <div className="col-6">
+                <div className="col-6 ml-5">
                     <div className="d-flex justify-content-between">
                         <p>123456</p>
                         <p>exemplo</p>
                     </div>
-                    <h1>DOG HOUSE</h1>
-                    <p>A comfortable bed for the dog. It will bring a lot of pleasure to your pet.</p>
-                    <p>High quality material does not allow the wool to stick and easy to clean.</p>
-                    <div className="d-flex justify-content-between">
-                        <h3>$ 37.99</h3>
-                        <div className="d-flex text-success">
+                    <h1 className="mt-5">DOG HOUSE</h1>
+                    <p>A comfortable bed for the dog. It will bring a lot of pleasure to your pet. High quality material does not allow the wool to stick and easy to clean.</p>
+                    <div className="product-price-container d-flex justify-content-between mt-4">
+                        <h3 className="mb-0">$ 37.99</h3>
+                        <div className="product-price-availability d-flex justify-content-between align-items-center text-success">
                             <FontAwesomeIcon icon="check" />
                             <p>Available</p>
                         </div>
                     </div>
-                    <div className="d-flex justify-content-between">
+                    <div className="product-details-container d-flex justify-content-between mt-4">
                         <div>
                             <p>Size</p>
-                            <select>
+                            <select className="mt-2">
                                 <option>100x100 cm</option>
                                 <option>200x200 cm</option>
                                 <option>300x300 cm</option>
@@ -49,39 +48,42 @@ const productCard = props => {
                         </div>
                         <div>
                             <p>Color</p>
-                            <div>
+                            <div className="mt-2">
                                 <input type="checkbox" />
                                 <input type="checkbox" />
                                 <input type="checkbox" />
                             </div>
                         </div>
                     </div>
-                    <div className="d-flex justify-content-between">
+                    <div className="product-qtde-container d-flex justify-content-between align-items-center mt-4">
                         <input type="number" name="tentacles" min="0"/>
                         <p>ADD TO BAG</p>     
                         <FontAwesomeIcon icon={["far", "heart"]} />
                     </div>
-                    <div>
+                    <div className="product-category-container mt-3">
                         <p>Category: <span className="font-weight-bold"> Living room</span></p>
                         <p>Tags: <span className="font-weight-bold"> Furniture, Decor</span></p>
                     </div>
-                    <div className="d-flex justify-content-between align-items-end">
-                        <div className="d-flex">
-                            <div>
+                    <div className="d-flex justify-content-between align-items-end mt-5">
+                        <div className="product-specifications-container d-flex flex-column">
+                            <div className="d-flex justify-content-between">
                                 <h6>DETAILS</h6>
-                                <div>
-                                    <p>Material <span className="font-weight-bold"> Polyester</span></p>
-                                    <p>Care <span className="font-weight-bold"> 30 degree wash</span></p>
-                                    <p>Size <span className="font-weight-bold"> 100x100 cm</span></p>
-                                </div>
-                            </div>
-                            <div>
                                 <h6>SPECIFICATIONS</h6>
+                            </div>
+                            <div className="d-flex justify-content-between">
                                 <div>
+                                    <p>Material:</p>
+                                    <p>Care:</p>
+                                    <p>Size:</p>
+                                </div>
+                                <div>
+                                    <p className="font-weight-bold"> Polyester</p>
+                                    <p className="font-weight-bold"> 30 degree wash</p>
+                                    <p className="font-weight-bold"> 100x100 cm</p>
                                 </div>
                             </div>
                         </div>
-                        <div className="d-flex justify-content-end">
+                        <div className="d-flex justify-content-end align-items-center">
                             <FontAwesomeIcon icon="share-alt" className="mr-4" />
                             <p>Share</p>
                         </div>
