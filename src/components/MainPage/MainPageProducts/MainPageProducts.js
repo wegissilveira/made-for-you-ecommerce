743a6d1d29d7646e1727485744ef42511d4c88e7
Wegis Silveira <wegis.h.silveira@gmail.com>
2020-08-20 20:13:13 -0300

    Commit final do dia

--- src/components/MainPage/MainPageProducts/MainPageProducts.js
+++ src/components/MainPage/MainPageProducts/MainPageProducts.js
@@ -5,85 +5,141 @@ import Products from '../../Shared/Products'
 import './MainPageProducts.css'
 
 
-const mainPageProducts = props => {
+const MainPageProducts = props => {
+
+    let [tag, setTag] = React.useState('all')
 
     let products = [
         {
             name: 'Produto',
             price: '38.99',
-            img: require('../../../assets/images/Products/molde.png')
+            img: require('../../../assets/images/Products/molde-textile.png'),
+            tag: 'textile'
         },
         {
             name: 'Produto',
             price: '38.99',
-            img: require('../../../assets/images/Products/molde.png')
+            img: require('../../../assets/images/Products/molde-furniture.png'),
+            tag: 'furniture'
         },
         {
             name: 'Produto',
             price: '38.99',
-            img: require('../../../assets/images/Products/molde.png')
+            img: require('../../../assets/images/Products/molde-decorations.png'),
+            tag: 'decorations'
         },
         {
             name: 'Produto',
             price: '38.99',
-            img: require('../../../assets/images/Products/molde.png')
+            img: require('../../../assets/images/Products/molde-furniture.png'),
+            tag: 'furniture'
         },
         {
             name: 'Produto',
             price: '38.99',
-            img: require('../../../assets/images/Products/molde.png')
+            img: require('../../../assets/images/Products/molde-decorations.png'),
+            tag: 'decorations'
         },
         {
             name: 'Produto',
             price: '38.99',
-            img: require('../../../assets/images/Products/molde.png')
+            img: require('../../../assets/images/Products/molde-furniture.png'),
+            tag: 'furniture'
         },
         {
             name: 'Produto',
             price: '38.99',
-            img: require('../../../assets/images/Products/molde.png')
+            img: require('../../../assets/images/Products/molde-textile.png'),
+            tag: 'textile'
         },
         {
             name: 'Produto',
             price: '38.99',
-            img: require('../../../assets/images/Products/molde.png')
+            img: require('../../../assets/images/Products/molde-furniture.png'),
+            tag: 'furniture'
         },
         {
             name: 'Produto',
             price: '38.99',
-            img: require('../../../assets/images/Products/molde.png')
+            img: require('../../../assets/images/Products/molde-textile.png'),
+            tag: 'textile'
         },
         {
             name: 'Produto',
             price: '38.99',
-            img: require('../../../assets/images/Products/molde.png')
+            img: require('../../../assets/images/Products/molde-decorations.png'),
+            tag: 'decorations'
         },
         {
             name: 'Produto',
             price: '38.99',
-            img: require('../../../assets/images/Products/molde.png')
+            img: require('../../../assets/images/Products/molde-decorations.png'),
+            tag: 'decorations'
         },
         {
             name: 'Produto',
             price: '38.99',
-            img: require('../../../assets/images/Products/molde.png')
+            img: require('../../../assets/images/Products/molde-furniture.png'),
+            tag: 'furniture'
+        },
+        {
+            name: 'Produto',
+            price: '38.99',
+            img: require('../../../assets/images/Products/molde-furniture.png'),
+            tag: 'furniture'
+        },
+        {
+            name: 'Produto',
+            price: '38.99',
+            img: require('../../../assets/images/Products/molde-furniture.png'),
+            tag: 'furniture'
+        },
+        {
+            name: 'Produto',
+            price: '38.99',
+            img: require('../../../assets/images/Products/molde-textile.png'),
+            tag: 'textile'
+        },
+        {
+            name: 'Produto',
+            price: '38.99',
+            img: require('../../../assets/images/Products/molde-decorations.png'),
+            tag: 'decorations'
         },
     ]
 
+    const setActiveTabHandler = e => {
 
+        let target = e.target
+        let children = e.currentTarget.children
+        let childrenArray = Array.from(children)
+
+        childrenArray.map(child => {
+            child.className = 'products-select'
+        })
+        
+        target.className = 'products-select products-select-active'
+
+    }
+    
+    // Seleção de aba funcionando perfeitamente, só falta inserir o ícone arrow abaixo da aba selecionada
     return (
         <div className="session-container text-center">
             <h1>FEATURED PRODUCTS</h1>
             <p>Select a category using special switches or go to the section with a convenient filter by product</p>
-            <div className="products-select-container d-flex justify-content-between mt-5">
-                <p className="products-select">ALL PRODUCTS</p>
-                <p className="products-select">FURNITURE</p>
-                <p className="products-select">DECORATION</p>
-                <p className="products-select">TEXTILE</p>
+            <div onClick={(e) => setActiveTabHandler(e)} className="products-select-container d-flex justify-content-between mt-5">
+                <p onClick={() => setTag('all')} className="products-select products-select-active">ALL PRODUCTS</p>
+                <p onClick={() => setTag('furniture')} className="products-select">FURNITURE</p>
+                <p onClick={() => setTag('decorations')}  className="products-select">DECORATIONS</p>
+                <p onClick={() => setTag('textile')}  className="products-select">TEXTILE</p>
             </div>
-            <Products products={products} pageLimit={8} />
+            <Products 
+                products={products} 
+                pageLimit={8} 
+                tag={tag}
+            />
         </div>
     )
 }
 
-export default mainPageProducts
+export default MainPageProducts
--- src/components/Shared/Products.js
+++ src/components/Shared/Products.js
@@ -7,16 +7,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const Products = props => {
 
     let [count, setCount] = React.useState(props.pageLimit)
-    let [disable, setDisable] = React.useState(false)
     
-    const setList = () => {
-        setCount(count + 4)
+    let products 
+    if (props.tag === 'all') {
+        products = props.products
+    } else {
+        products = props.products.filter(item => item.tag === props.tag)
+    }  
 
-        if (count + 4 >= props.products.length) {
-            setDisable(true)
-        }
-    }
-    
 
     return (
         <div className="session-container container">
@@ -24,39 +22,54 @@ const Products = props => {
                 {/* Corrigir key */}
                 {/* Funcionando perfeitamente, mas ainda falta melhorar a transição quando clicamos em 'SHOW MORE' */}
                 {
-                    props.products.map((product, i) => {
+                    products.map((product, i) => {
                         let productsList
-                        if(i + 1 <= count) {
-                            productsList = <div className=" col-3 mt-4 pl-0" key={product+i}>
-                                                <div className="border p-0">
-                                                    <img src={product.img} alt="Produto" style={{maxWidth: '100%'}} />
-                                                    <div className="products-description d-flex justify-content-between align-items-center">
-                                                        <div className="d-flex flex-column align-items-start products-description-icons">
-                                                            <p>{product.name}</p>
-                                                            <p>$ {product.price}</p>
-                                                        </div>
-                                                        <div className="d-flex justify-content-between align-items-center products-description-icons">
-                                                            <FontAwesomeIcon icon="eye" />
-                                                            <FontAwesomeIcon icon="suitcase" size="2x" />
+                        if (i + 1 <= count) {
+                            if (props.tag === 'all' || product.tag === props.tag) {
+                                productsList = <div className=" col-3 mt-4 pl-0" key={product+i}>
+                                                    <div className="border p-0">
+                                                        <img src={product.img} alt="Produto" style={{maxWidth: '100%'}} />
+                                                        <div className="products-description d-flex justify-content-between align-items-center">
+                                                            <div className="d-flex flex-column align-items-start products-description-icons">
+                                                                <p>{product.name}</p>
+                                                                <p>$ {product.price}</p>
+                                                            </div>
+                                                            <div className="d-flex justify-content-between align-items-center products-description-icons">
+                                                                <FontAwesomeIcon icon="eye" />
+                                                                <FontAwesomeIcon icon="suitcase" size="2x" />
+                                                            </div>
                                                         </div>
                                                     </div>
                                                 </div>
-                                            </div>
+                            }
                         }
                         return productsList
                     })
                 }
 
             </div>
-            <div className="products-showmore">
-                <div className="products-showmore-text">
-                    <button 
-                        disabled={disable} 
-                        type="button" 
-                        className="btn" 
-                        onClick={() => setList()}
-                        > SHOW MORE
-                    </button>
+            <div className="d-flex justify-content-between products-show-container">
+                <div className="products-show">
+                    <div className="products-show-text">
+                        <button 
+                            disabled={count >= products.length} 
+                            type="button" 
+                            className="btn border-success" 
+                            onClick={() => setCount(count + 4)}
+                            > SHOW MORE
+                        </button>
+                    </div>
+                </div>
+                <div className="products-show">
+                    <div className="products-show-text">
+                        <button 
+                            disabled={count <= props.pageLimit || products.length <= props.pageLimit} 
+                            type="button" 
+                            className="btn border-danger" 
+                            onClick={() => setCount(count - 4)}
+                            > SHOW MENOS
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
