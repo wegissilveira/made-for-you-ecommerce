f693c1429ebc16305bac522f966178147aedae90
Wegis Silveira <wegis.h.silveira@gmail.com>
2020-09-02 18:23:03 -0300

    commit protocolar

--- src/containers/MainPageConfig/MainPageConfig.js
+++ src/containers/MainPageConfig/MainPageConfig.js
@@ -3,7 +3,7 @@ import React, {Component, Fragment} from 'react'
 import './MainPageConfig.css'
 
 import MainPageHeader from '../../components/MainPage/MainPageHeader/MainPageHeader'
-import MainPageShopByRoom from '../../components/MainPage/MainPageShop/MainPageShopByRoom'
+import MainPageShopByRoom from '../../components/MainPage/MainPageShopByRoom/MainPageShopByRoom'
 import MainPageProducts from '../../components/MainPage/MainPageProducts/MainPageProducts'
 import MainPageDiscounts from '../../components/MainPage/MainPageDiscounts/MainPageDiscounts'
 
