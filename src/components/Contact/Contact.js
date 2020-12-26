92af623a73ba1bfc4c210bb1016386bcec224e21
Wegis Silveira <wegis.h.silveira@gmail.com>
2020-12-26 17:10:53 -0300

    Responsividade: Contatos - FINALIZADO; Contatos - Remoção de bootstrap - FINALIZADO

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,25 +9,33 @@ const Contact = props => {
     return (
         <div className={classes.Session_container}>
             <div className={classes.Contact_box_container}>
-                <div className={`row ${classes.Contact_box}`}>
-                    <div className={`col-5 ${classes.Contact_box_info}`}>
-                        <div className={classes.Contact_box_info_sub}>
+                <div>
+                    <div className={classes.Contact_box_info}>
+                        <div>
                             <h4>Get in Touch</h4>
-                            <p>Feel free to contact us about our services or any questions you may heve. We will get back to you as soon as possible.</p>
-                            <div className="d-flex align-items-center">
-                                <FontAwesomeIcon icon="envelope" size="2x" style={{color:'grey'}}/>
-                                <p className="ml-3 mb-0">+375 29 364-74-69</p>
+                            <p>Feel free to contact us about our services or any questions you may have. We will get back to you as soon as possible.</p>
+                            <div>
+                                <FontAwesomeIcon 
+                                    icon="envelope" 
+                                    size="2x" 
+                                    style={{color:'grey'}}
+                                />
+                                <p>+375 29 364-74-69</p>
                             </div>
-                            <div className="d-flex align-items-center">
-                                <FontAwesomeIcon icon="phone-alt" size="2x" style={{color:'grey'}}/>
-                                <p className="ml-3 mb-0">hello@myhome.com</p>
+                            <div>
+                                <FontAwesomeIcon 
+                                    icon="phone-alt" 
+                                    size="2x" 
+                                    style={{color:'grey'}}
+                                    />
+                                <p>hello@myhome.com</p>
                             </div>
                         </div>
                     </div>
-                    <div className="col-7">
-                        <div className={classes.Contact_box_form_sub}>
+                    <div className={classes.Contact_box_form}>
+                        <div>
                             <h4>Send us a message</h4>
-                            <div className="d-flex justify-content-between">
+                            <div>
                                 <input placeholder="First name"/>
                                 <input placeholder="Last name"/>
                             </div>
